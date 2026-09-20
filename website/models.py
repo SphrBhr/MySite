07f747d0bf6f7728c39f0483c30a3077b9a59588
sepehr <sepehrbahrian83@gmail.com>
2026-09-20 14:54:56 +0330

    django form

--- website/models.py
+++ website/models.py
@@ -11,6 +11,10 @@ class Contact(models.Model):
     
     class Meta:
         ordering = ['created_date']
-        
     def __str__(self):
         return self.name
+    
+class Newsletter(models.Model):
+    email = models.EmailField()
+    def __str__(self):
+        return self.email
